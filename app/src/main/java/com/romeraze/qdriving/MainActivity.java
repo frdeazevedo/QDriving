0222ddd16bd3b29a938f6ac19268b2c1ffd232e8
frdeazevedo <sgsfabiano@gmail.com>
2017-01-15 01:10:39 -0200

    Addded question list and rotation.

--- app/src/main/java/com/romeraze/qdriving/MainActivity.java
+++ app/src/main/java/com/romeraze/qdriving/MainActivity.java
@@ -1,6 +1,8 @@
 package com.romeraze.qdriving;
 
+import android.content.res.Configuration;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +12,16 @@ import android.widget.EditText;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, View.OnClickListener{
     Button button_correct, button_wrong, button_repeat;
     TextToSpeech tts;
     Question question;
+    List<Question> questionList;
+    int currentQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,25 +48,65 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
     @Override
     public void onClick(View v) {
+        if(currentQuestion > questionList.size()) {
+            currentQuestion = 0;
+        }
+
+        question = questionList.get(currentQuestion);
+
         switch(v.getId()) {
             case R.id.button_repeat:
                 tts.speak(question.getQuestion(), TextToSpeech.QUEUE_FLUSH, null);
                 break;
             case R.id.button_correct:
+                this.currentQuestion++;
                 if(question.answerTheQuestion(true)) {
                     tts.speak("Você acertou!", TextToSpeech.QUEUE_FLUSH, null);
                 } else {
                     tts.speak("Você errou!", TextToSpeech.QUEUE_FLUSH, null);
                 }
+
+                while(tts.isSpeaking()) {
+                }
+
+                tts.speak("Próxima questão!", TextToSpeech.QUEUE_FLUSH, null);
+
+                while(tts.isSpeaking()) {
+                }
+
+                getNextQuestion();
+
+                tts.speak(question.getQuestion(), TextToSpeech.QUEUE_FLUSH, null);
                 break;
             case R.id.button_wrong:
+                this.currentQuestion++;
                 if(question.answerTheQuestion(false)) {
                     tts.speak("Você acertou!", TextToSpeech.QUEUE_FLUSH, null);
                 } else {
                     tts.speak("Você errou!", TextToSpeech.QUEUE_FLUSH, null);
                 }
+
+                while(tts.isSpeaking()) {
+                }
+
+                tts.speak("Próxima questão!", TextToSpeech.QUEUE_FLUSH, null);
+
+                while(tts.isSpeaking()) {
+                }
+
+                getNextQuestion();
+
+                tts.speak(question.getQuestion(), TextToSpeech.QUEUE_FLUSH, null);
                 break;
         }
+    }
+
+    private void getNextQuestion() {
+        if(currentQuestion >= questionList.size()) {
+            currentQuestion = 0;
+        }
+
+        question = questionList.get(currentQuestion);
     }
 
     private void initGUI() {
@@ -78,6 +124,13 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     }
 
     private void initQuestions() {
-        question = new Question("Cargo em comissão é de livre nomeação e exoneração", true);
+        currentQuestion = 0;
+
+        questionList = new ArrayList<Question>();
+        questionList.add(new Question("O território aduaneiro compreende todo o território nacional", true));
+        questionList.add(new Question("Duplicatas descontadas são classificadas no Passivo", true));
+        questionList.add(new Question("Servidor público não pode ser demitido em nenhuma hipótese", false));
+        questionList.add(new Question("O servidor nomeado tem 15 dias para tomar posse", false));
+        questionList.add(new Question("Despesas diferidas têm a mesma natureza de despesas antecipadas", true));
     }
 }
